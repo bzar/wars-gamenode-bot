a33196e271deb575fcc44c882f6384ed7edf9c2c
Teemu Erkkola <teemu.erkkola@iki.fi>
2012-09-26 23:04:33 +0300

    Slfix typo

--- src/manager.js
+++ src/manager.js
@@ -6,14 +6,14 @@ function Manager(client) {
 };
 
 Manager.prototype.playerJoined = function(gameId, playerNumber, playerName, isMe) {
-  var handler = handlers[gameId];
+  var handler = this.handlers[gameId];
   if(handler !== undefined) {
     handler.playerJoined(playerNumber, playerName, isMe);
   }
 }
 
 Manager.prototype.playerLeft = function(gameId, playerNumber) {
-  var handler = handlers[gameId];
+  var handler = this.handlers[gameId];
   if(handler !== undefined) {
     handler.playerLeft(playerNumber);
   }
@@ -26,28 +26,28 @@ Manager.prototype.unitUnbanned = function(unitType) {
 }
 
 Manager.prototype.gameStarted = function(gameId) {
-  var handler = handlers[gameId];
+  var handler = this.handlers[gameId];
   if(handler !== undefined) {
     handler.gameStarted();
   }
 }
 
 Manager.prototype.gameFinished = function(gameId) {
-  var handler = handlers[gameId];
+  var handler = this.handlers[gameId];
   if(handler !== undefined) {
     handler.gameFinished();
   }
 }
 
 Manager.prototype.gameTurnChange = function(gameId, newTurn, newRound, turnRemaining) {
-  var handler = handlers[gameId];
+  var handler = this.handlers[gameId];
   if(handler !== undefined) {
     handler.gameTurnChange(newTurn, newRound, turnRemaining);
   }
 }
 
 Manager.prototype.gameEvents = function(gameId, events) {
-  var handler = handlers[gameId];
+  var handler = this.handlers[gameId];
   if(handler !== undefined) {
     handler.gameEvents(events);
   }
